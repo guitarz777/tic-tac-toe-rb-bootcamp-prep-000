5dc46a52f20aaa75ab593d457fd4684e21c94117
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-14 01:51:19 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,7 +23,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
+def player_move(board, index, current_player)
   board[index] = current_player
 end
 
