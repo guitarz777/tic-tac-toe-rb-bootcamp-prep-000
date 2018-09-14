49f9c0260c0f194f54291908c01d71d52b56106a
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-14 02:15:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,3 +55,26 @@ def play(board)
     turn += 1
   end
 end
+
+
+def turn_count(board)
+  count = 0
+  board.each do |i|
+    if i == "X" || i == "O"
+      count += 1
+    end
+  end
+end
+
+
+def current_player()
+  if turn_count(board) % 2 = 0 
+    return "X"
+  else
+    return "O"
+  end
+end
+
+
+  
+  
